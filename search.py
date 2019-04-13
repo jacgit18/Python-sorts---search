81ef8f2b6b2847246a02871aa5f4f4f6a2b5268b
Jac <joshuacarpentier21@gmail.com>
2019-04-13 05:47:13 -0400

    removed comments

--- search.py
+++ search.py
@@ -18,26 +18,4 @@ if linear_search(numbers, n):
     print("found at ", position + 1)
 else:
     print("lost")
-# position = -1
 
-
-# def search(list1, n):
-#    i = 1
-
-#   while i < len(list1):
-#        if list1[i] == n:
-#            globals()['position'] = i
-#            return True
-#        i = i + 1
-
-#    return False
-
-
-# list1 = [1, 5, 9, 3, 4, 6]
-# n = 6
-
-# if n in list1:
-#    print("found at ", position + 1)
-# # print("found")
-# else:
-#    print("not found")
